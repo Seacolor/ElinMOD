ae3e15dd6fff23060c91b8b22877f7ffd7526f93
Kazushi Tominaga <github@seacolorswind.com>
2024-12-06 12:52:51 +0900

    Fixed a mistake in version specification.

--- PetOperation/ModPetOperation.cs
+++ PetOperation/ModPetOperation.cs
@@ -4,7 +4,7 @@ using HarmonyLib;
 
 namespace PetOperation
 {
-    [BepInPlugin("com.seacolorswind.pet_operation", "Pet Operation", "1.0.0")]
+    [BepInPlugin("com.seacolorswind.pet_operation", "Pet Operation", "1.0.2")]
     public class ModPetOperation : BaseUnityPlugin
     {
         private void Start() {
