088b93ac490515c62a38567c930479e82bc1ac87
Kazushi Tominaga <github@seacolorswind.com>
2025-06-28 19:13:47 +0900

    Released version 1.1.0 of DanalinOfWater.

--- DanalinOfWater/ModDanalinOfWater.cs
+++ DanalinOfWater/ModDanalinOfWater.cs
@@ -4,7 +4,7 @@ using HarmonyLib;
 
 namespace DanalinOfWater
 {
-    [BepInPlugin("com.seacolorswind.danalin_of_water", "Danalin of Water", "1.0.1")]
+    [BepInPlugin("com.seacolorswind.danalin_of_water", "Danalin of Water", "1.1.0")]
     public class ModDanalinOfWater : BaseUnityPlugin
     {
         private void Awake()
